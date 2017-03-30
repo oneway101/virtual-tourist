ab97a1386a8ad25a5a3fb066e3dced5bd40de3e1
oneway101 <oneway.first@gmail.com>
2017-03-30 11:32:04 -0400

    clean up

--- virtual-tourist/FlickrClient.swift
+++ virtual-tourist/FlickrClient.swift
@@ -9,32 +9,24 @@
 import UIKit
 
 class FlickrClient: NSObject {
-    
-    func searchImages(_ sender: AnyObject) {
-            let methodParameters = [
-                Constants.FlickrParameterKeys.Method: Constants.FlickrParameterValues.SearchMethod,
-                Constants.FlickrParameterKeys.APIKey: Constants.FlickrParameterValues.APIKey,
-                Constants.FlickrParameterKeys.BoundingBox: bboxString(),
-                Constants.FlickrParameterKeys.SafeSearch: Constants.FlickrParameterValues.UseSafeSearch,
-                Constants.FlickrParameterKeys.Extras: Constants.FlickrParameterValues.MediumURL,
-                Constants.FlickrParameterKeys.Format: Constants.FlickrParameterValues.ResponseFormat,
-                Constants.FlickrParameterKeys.NoJSONCallback: Constants.FlickrParameterValues.DisableJSONCallback
-            ]
-            getImagesFromFlickr(methodParameters as [String:AnyObject])
 
-    }
+    let methodParameters = [
+        Constants.FlickrParameterKeys.Method: Constants.FlickrParameterValues.SearchMethod,
+        Constants.FlickrParameterKeys.APIKey: Constants.FlickrParameterValues.APIKey,
+        Constants.FlickrParameterKeys.BoundingBox: bboxString(),
+        Constants.FlickrParameterKeys.SafeSearch: Constants.FlickrParameterValues.UseSafeSearch,
+        Constants.FlickrParameterKeys.Extras: Constants.FlickrParameterValues.MediumURL,
+        Constants.FlickrParameterKeys.Format: Constants.FlickrParameterValues.ResponseFormat,
+        Constants.FlickrParameterKeys.NoJSONCallback: Constants.FlickrParameterValues.DisableJSONCallback
+    ]
+
     
     private func bboxString() -> String {
-        // ensure bbox is bounded by minimum and maximums
-        if let latitude = Double(latitudeTextField.text!), let longitude = Double(longitudeTextField.text!) {
-            let minimumLon = max(longitude - Constants.Flickr.SearchBBoxHalfWidth, Constants.Flickr.SearchLonRange.0)
-            let minimumLat = max(latitude - Constants.Flickr.SearchBBoxHalfHeight, Constants.Flickr.SearchLatRange.0)
-            let maximumLon = min(longitude + Constants.Flickr.SearchBBoxHalfWidth, Constants.Flickr.SearchLonRange.1)
-            let maximumLat = min(latitude + Constants.Flickr.SearchBBoxHalfHeight, Constants.Flickr.SearchLatRange.1)
-            return "\(minimumLon),\(minimumLat),\(maximumLon),\(maximumLat)"
-        } else {
-            return "0,0,0,0"
-        }
+        let minimumLon = max(longitude - Constants.Flickr.SearchBBoxHalfWidth, Constants.Flickr.SearchLonRange.0)
+        let minimumLat = max(latitude - Constants.Flickr.SearchBBoxHalfHeight, Constants.Flickr.SearchLatRange.0)
+        let maximumLon = min(longitude + Constants.Flickr.SearchBBoxHalfWidth, Constants.Flickr.SearchLonRange.1)
+        let maximumLat = min(latitude + Constants.Flickr.SearchBBoxHalfHeight, Constants.Flickr.SearchLatRange.1)
+        return "\(minimumLon),\(minimumLat),\(maximumLon),\(maximumLat)"
     }
     
     // MARK: Flickr API
